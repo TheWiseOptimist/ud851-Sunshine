96e4d37d44e8d089852bd58abe1a6205da5bb437
clyde <clyder@thewiseoptimist.net>
2018-05-29 13:56:53 -0700

    Sunshine S04.02-Exercise-DisplayDayForecast Lesson 4.24 todo's completed Add safety measures for getting intent data.

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-
+    private String weatherForDay;
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
     @Override
@@ -16,7 +16,12 @@ public class DetailActivity extends AppCompatActivity {
 
         // TODO completed (2) Display the weather forecast that was passed from MainActivity
         Intent intentReceivedFromMainActivity = getIntent();
-        String weatherForDay = intentReceivedFromMainActivity.getStringExtra(Intent.EXTRA_TEXT);
+        // The if statements implement safety precautions in retrieving the intent data.
+        if (intentReceivedFromMainActivity != null
+//                && intentReceivedFromMainActivity.getStringExtra(Intent.EXTRA_TEXT) != null) {
+                && intentReceivedFromMainActivity.hasExtra(Intent.EXTRA_TEXT)) {
+            weatherForDay = intentReceivedFromMainActivity.getStringExtra(Intent.EXTRA_TEXT);
+        }
         TextView detailTextView = findViewById(R.id.detail_text_view);
         detailTextView.setText(weatherForDay);
     }
